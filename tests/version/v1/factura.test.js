f589ff14209e484374328ccda7eabd34791085f4
Juan Jose Duran Rincon <jduranrinconcampus@gmail.com>
2023-09-04 08:56:55 -0500

    feat: :construction: Funciones de testeo: Ejercicio 7
    Se crea las funciones de testeo para el ejercicio 7 , donde se prueban el correcto funcionamiento del script

--- tests/version/v1/factura.test.js
+++ tests/version/v1/factura.test.js
@@ -1,9 +1,21 @@
-import { obtener } from "../../../version/v1/factura";
+import { factura } from "../../../version/v1/factura";
 
 describe('test del archivo factura.js', () => { 
-    let app = obtener("Fred");
-    let obj = {"id": 1, "nombre": "Fred"}
-    test('El objeto esperado { ....id, ...nombre}', () => { 
+    let app = factura("Juan Jose", 106152664, "PLAY5", 10000000, 3);
+    test('Resultado esperado al enviar la siguiente data ("Juan Jose", 106152664, "PLAY5", 10000000, 3)', () => { 
+        let Nombre = "Juan Jose";
+        let ID = 106152664;
+        let NA = "PLAY5";
+        let PA = 10000000;
+        let CA = 3;
+        let Total = CA * PA;
+        let obj = { 
+            factura : "--------------FACTURA--------------",
+            informacion : `TIENDA ORIGINAL  NIT: 2344234234 \n \t Tienda Sur TEL: ########## \n \t Comprador: ${Nombre} \n \t CC: ${ID} \n \tCODIGO----DESCRIPCION----UNIDADES----VALOR`,
+            producto: ` 1987     ${NA}      ${CA}      ${PA}`,
+            totalS : "--------------TOTAL--------------",
+            totalT : `Total a pagar      COP$${Total}`
+        }
         expect(app).toStrictEqual(obj);
      })
      test('El objeto esperado es "Object"', () => { 
